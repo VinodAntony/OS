54dc997979eb288faf41b7e67e2d0a343be8f1f1
VinodAntony <antonyvinod@rocketmail.com>
2020-10-16 10:45:25 +0530

    added makefile

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OS
-TESTING
+TESTING	
+##  added makefile 
---
+++ buildprocess/builddemo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int main(int argc, char const *argv[])
+{
+	printf("addition result = %d \n\n",add(10,10));
+	printf("subtraction result = %d \n\n",sub(10,10));
+
+	return 0;
+}
