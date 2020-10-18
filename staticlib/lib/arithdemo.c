70ab15256f2b33e8148998fc874e361b5fdd062a
VinodAntony <antonyvinod@rocketmail.com>
2020-10-18 18:57:50 +0530

    added dynamic lib

---
+++ staticlib/lib/arithdemo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "arith.h"
+
+int main(int argc, char const *argv[])
+{
+	printf("addition result = %d \n\n",add(10,10));
+	printf("subtraction result = %d \n\n",sub(10,10));
+
+	return 0;
+}
