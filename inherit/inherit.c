b93858b48cb2c45ff7651727ddcba6e11434b8a5
VinodAntony <antonyvinod@rocketmail.com>
2020-10-31 13:39:59 +0530

    zombie process

---
+++ inherit/inherit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+	pid_t id; 
+	id = fork();
+
+	if(id == 0){
+		//child
+		printf("Child starting\n");
+		sleep(12);
+		printf("child exiting\n");
+	}
+	else{
+		//parent
+		printf("parent starting\n");
+		sleep(2);
+		printf("parent exiting\n");
+	}
+	return 0;
+}
